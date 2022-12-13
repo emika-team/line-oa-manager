105a71dc0aa8ff9ec75662d556347bf3942afd06
Tanapon Nanta <pangpond556_@hotmail.com>
2022-12-13 17:31:27 +0700

    seperate struct

---
+++ internal/firebase/models/emoji.go
@@ -0,0 +1,7 @@
+package models
+
+type Emoji struct {
+	Index     int    `json:"index"`
+	ProductID string `json:"productId"`
+	EmojiID   string `json:"emojiId"`
+}
--- internal/firebase/models/message.go
+++ internal/firebase/models/message.go
@@ -6,18 +6,12 @@ import (
 	"github.com/emika-team/line-oa-manager/internal/firebase"
 )
 
-type emoji struct {
-	Index     int    `json:"index"`
-	ProductID string `json:"productId"`
-	EmojiID   string `json:"emojiId"`
-}
-
 type Message struct {
 	Destination        string  `json:"destination"`
 	UID                string  `json:"uid"`
 	Type               string  `json:"type"`
 	Text               string  `json:"text"`
-	Emojis             []emoji `json:"emojis"`
+	Emojis             []Emoji `json:"emojis"`
 	PackageID          string  `json:"packageId"`
 	StickerID          string  `json:"stickerId"`
 	OriginalContentUrl string  `json:"originalContentUrl"`
